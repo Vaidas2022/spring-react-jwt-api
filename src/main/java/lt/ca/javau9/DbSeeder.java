d1ac6f5beaee789d228f9c50c9c12feb70152ee3
Vaidas Cesonis <vaices1974@gmail.com>
2024-10-08 11:04:52 +0300

    Added logger to DbSeeder

---
+++ src/main/java/lt/ca/javau9/DbSeeder.java
@@ -0,0 +1,33 @@
+package lt.ca.javau9;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import lt.ca.javau9.models.Role;
+import lt.ca.javau9.controllers.AuthController;
+import lt.ca.javau9.models.ERole;
+import lt.ca.javau9.repositories.RoleRepository;
+
+
+@Component
+public class DbSeeder implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(DbSeeder.class);
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (roleRepository.findByName(ERole.ROLE_ADMIN).isEmpty()) {
+            roleRepository.save(new Role(ERole.ROLE_ADMIN));
+            logger.info("ROLE_ADMIN added.");
+        }
+        
+        if (roleRepository.findByName(ERole.ROLE_USER).isEmpty()) {
+            roleRepository.save(new Role(ERole.ROLE_USER));
+            logger.info("ROLE_USER added.");
+        }
+    }
+}
